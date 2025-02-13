5234fb1ecfec360d96ef04edb6f776a2a59e151d
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2025-02-13 02:25:51 -0500

    added index code

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Industry API!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
